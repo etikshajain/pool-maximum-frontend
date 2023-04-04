9c69e2b52324abd492da0bf104b6826db27414de
etikshajain <jainetiksha@gmail.com>
2023-04-04 17:31:14 +0530

    deposit and withdraw from yearn vault, claim prize

--- src/components/PrizePools.jsx
+++ src/components/PrizePools.jsx
@@ -12,14 +12,14 @@ import ERC20_ABI from './Abi/ERC20.json';
 import Loaderr from "./Loaderr.jsx"
 
 let url = "HTTP://127.0.0.1:7545";
-let customHttpProvider = new ethers.providers.JsonRpcProvider(url);
-const PRIZE_POOL_CONTRACT_ADDRESS = "0xC95D3467C935a41cF7E0758203475e41F8dD6Ac6";
-const POOL_CONTRACT_ADDRESS = "0xc157eA17d93d34ec9Ef6D881b0Ad9587F229EB83";
+let customHttpProvider = new ethers.providers.JsonRpcProvider(url)
+const PRIZE_POOL_CONTRACT_ADDRESS = "0x2789c2c7A449a3749597748D500B1C6E78E18D41";
+const POOL_CONTRACT_ADDRESS = "0xAC9bea429642C77F4D8412fE8AA312436A97Df64";
 const TICKET_CONTRACT_ADDRESS = "0x67b82280bd0D2d3C94287EF7755316d4b8D89C14";
 const TOKEN_CONTRACT_ADDRESS_ETH = "0x2170ed0880ac9a755fd29b2688956bd959f933f8";
 const TOKEN_CONTRACT_ADDRESS_WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 ";
 const PRIZE_POOL_OWNER_ADDRESS = "0x6370Bd80276265Bbb648371DA719793EA3e964A6";
-const YEARN_VAULT_ADDRESS = "0xe1237aA7f535b0CC33Fd973D66cBf830354D16c7";
+const YEARN_VAULT_ADDRESS = "0xe1237aA7f535b0CC33Fd973D66cBf830354D16c7"
 
 // sleep function for testing
 function sleep(milliseconds) {
@@ -84,7 +84,7 @@ const PrizePools = (props) => {
         setTotalDeposits(ethers.utils.formatEther(td))
 
         // fetch your deposits in the pool
-        console.log("fetching your deposits")
+        console.log("fetching your deposits");
         let yd = await connectedContract.deposits(your_address);
         console.log(ethers.utils.formatEther(yd))
         setYourDeposit(ethers.utils.formatEther(yd))
@@ -102,26 +102,26 @@ const PrizePools = (props) => {
         // fetch total deposits in the pool
         console.log("fetching total deposits")
         let td = await connectedContract.totalDeposits();
-        console.log(td.toNumber())
-        setTotalDeposits(td.toNumber())
+        console.log(ethers.utils.formatEther(td))
+        setTotalDeposits(ethers.utils.formatEther(td))
 
         // fetch total tickets in the pool
         console.log("fetching total tickets")
         let tt = await connectedContract.totalTicketSupply();
-        console.log(tt.toNumber())
-        setTicketsCirculated(tt.toNumber())
+        console.log(ethers.utils.formatEther(tt))
+        setTicketsCirculated(ethers.utils.formatEther(tt))
 
         // fetch your deposits in the pool
         console.log("fetching your deposits")
         let yd = await connectedContract.deposits(your_address);
-        console.log(yd.toNumber())
-        setYourDeposit(yd.toNumber())
+        console.log(ethers.utils.formatEther(yd))
+        setYourDeposit(ethers.utils.formatEther(yd))
 
         // fetch your tickets in the pool
         console.log("fetching your tickets")
         let yt = await connectedContract.tickets(your_address);
-        console.log(yt.toNumber())
-        setYourTickets(yt.toNumber())
+        console.log(ethers.utils.formatEther(yt))
+        setYourTickets(ethers.utils.formatEther(yt))
 
         return 0;
         } catch (error) {
@@ -136,42 +136,42 @@ const PrizePools = (props) => {
         // fetch total deposits in the pool
         console.log("fetching total deposits")
         let td = await connectedContract.totalDeposits();
-        console.log(td.toNumber())
-        setTotalDeposits(td.toNumber())
+        console.log(ethers.utils.formatEther(td))
+        setTotalDeposits(ethers.utils.formatEther(td))
 
         // fetch total tickets in the pool
         console.log("fetching total tickets")
         let tt = await connectedContract.totalTicketSupply();
-        console.log(tt.toNumber())
-        setTicketsCirculated(tt.toNumber())
+        console.log(ethers.utils.formatEther(tt))
+        setTicketsCirculated(ethers.utils.formatEther(tt))
 
         // fetch your deposits in the pool
         console.log("fetching your deposits")
         let yd = await connectedContract.deposits(your_address);
-        console.log(yd.toNumber())
-        setYourDeposit(yd.toNumber())
+        console.log(ethers.utils.formatEther(yd))
+        setYourDeposit(ethers.utils.formatEther(yd))
 
         // fetch your tickets in the pool
         console.log("fetching your tickets")
         let yt = await connectedContract.tickets(your_address);
-        console.log(yt.toNumber())
-        setYourTickets(yt.toNumber())
+        console.log(ethers.utils.formatEther(yt))
+        setYourTickets(ethers.utils.formatEther(yt))
 
         // fetch total interest yo
         console.log("fetching total interest")
         let ti = await connectedContract.totalInterest();
-        console.log(ti.toNumber())
-        setTotalInterest(ti.toNumber())
+        console.log(ethers.utils.formatEther(ti))
+        setTotalInterest(ethers.utils.formatEther(ti))
 
         // fetch your prize
         console.log("fetching your prize")
         let yp = await connectedContract.calculateInterest(your_address);
-        console.log(yp.toNumber())
-        setYourPrize(yp.toNumber())
+        console.log(ethers.utils.formatEther(yp))
+        setYourPrize(ethers.utils.formatEther(yp))
 
         // fetch winner or not
         console.log("fetching whether winner")
-        let w = await connectedContract.checkWinner();
+        let w = await connectedContract.checkwinner();
         console.log(w)
         setWinner(w)
 
@@ -190,7 +190,7 @@ const PrizePools = (props) => {
       
       console.log("checking that you have enough eth")
       const balance = await customHttpProvider.getBalance(your_address);
-      const balanceInEth = ethers.utils.formatEther(balance)
+      const balanceInEth = ethers.utils.formatEther(balance);
       console.log("your eth balance is", balanceInEth)
 
       if(balance>=amount){
@@ -212,7 +212,7 @@ const PrizePools = (props) => {
       }
   }
 
-    const { address } = useAccount()
+    const { address } = useAccount();
     const [contract, setContract] = useState([]);
     const [totalDeposits, setTotalDeposits] = useState(0);
     const [ticketsCirculated, setTicketsCirculated] = useState(0);
@@ -227,21 +227,37 @@ const PrizePools = (props) => {
     const [withdrawTime,setWithdrawTime]=useState(0);
     const [loading,setLoading]=useState(true);
 
-    const handleOnChange = (e)=>{
-    setNewDeposit({...newDeposit,[e.target.name]:e.target.value});
-    }
-    const handleClick = (e)=>{
-      console.log("clicked")
-    // editNote(note.id,note.etitle,note.edescription,note.etag);
-    }
-
     const depositFunds = async (e)=>{
       // e.preventDefault()
-      console.log("depositing")
-      await depositToPrizePool(address, 10, e);
+      console.log("depositing");
+      await depositToPrizePool(address, 1, e);
     }
-    const claimPrize = (e)=>{
-      console.log("claim")
+    const claimPrize = async (e)=>{
+      console.log("claiming");
+      const connectedContract = await getContract(PRIZE_POOL_CONTRACT_ADDRESS, PRIZE_POOL_ABI.abi, customHttpProvider)
+      await connectedContract.claim(); 
+    }
+
+    const depositToVault = async (e)=>{
+      console.log("deposit to vault");
+      const connectedContract = await getContract(PRIZE_POOL_CONTRACT_ADDRESS, PRIZE_POOL_ABI.abi, customHttpProvider)
+      await connectedContract.depositToYv();
+    }
+
+    const withdrawFromVault = async (e)=>{
+      console.log("withdraw from vault");
+      const connectedContract = await getContract(PRIZE_POOL_CONTRACT_ADDRESS, PRIZE_POOL_ABI.abi, customHttpProvider)
+      // send 0.1 eth to the prize pool
+      console.log("sending 0.1 ether");
+      const signer = customHttpProvider.getSigner(address);
+      const tx = {
+          to: PRIZE_POOL_CONTRACT_ADDRESS,
+          value: ethers.utils.parseEther('0.1', 'ether'),
+          gasLimit: 50000,
+      };
+      const transaction = await signer.sendTransaction(tx);
+      console.log("sent");
+      await connectedContract.withdrawFromYv()
     }
 
   useEffect( () => {
@@ -257,7 +273,7 @@ const PrizePools = (props) => {
         const withdraw_time = await pp_contract.nextDrawTime();
         setWithdrawTime(withdraw_time.toNumber()*1000);
         console.log("here")
-        console.log(contract);
+        console.log(contract)
         console.log(tvl);
         console.log(depositDeadline);
         console.log(withdrawTime);
@@ -287,37 +303,53 @@ const PrizePools = (props) => {
     <br/>
     <span className="drawTimer">Total Value locked is ...{TVL}</span>
     <h2>Prize Pools</h2>
-    <div className="container text-center prizePoolContainer">
-    <ul>
-      <li>Chain: Ethereum</li>
-      <li>Prize Pool address: {PRIZE_POOL_CONTRACT_ADDRESS}</li>
-      <li>Ticket address: {TICKET_CONTRACT_ADDRESS}</li>
-      <li>Prize Pool token: {TOKEN_CONTRACT_ADDRESS_ETH}</li>
-      <li>Deposit Deadline: {depositDeadline}</li>
-      <li>Withdrawal Time: {withdrawTime}</li>
-      <li>Yearn Vault Utilised: {YEARN_VAULT_ADDRESS}</li>
-      <li>Award Share for winner: 80%</li>
+    <table className="container">
+    <span class="badge text-bg-dark">Deposit Deadline: {depositDeadline}</span>
+    <br/>
+    <span class="badge text-bg-dark">Withdrawal Time: {withdrawTime}</span>
+    {state==="Closed"?<><br/><span class="badge text-bg-light">{winner?"Yes":"No"}</span></>:<></>}
+      <tr><th>Chain</th> <td>Ethereum</td></tr>
+      <tr><th>Prize Pool address</th> <td>{PRIZE_POOL_CONTRACT_ADDRESS}</td></tr>
+      <tr><th>Ticket address</th> <td>{TICKET_CONTRACT_ADDRESS}</td></tr>
+      <tr><th>Prize Pool token</th> <td>{TOKEN_CONTRACT_ADDRESS_ETH}</td></tr>
+      <tr><th>Yearn Vault Utilised</th> <td>{YEARN_VAULT_ADDRESS}</td></tr>
+      <tr><th>Award Share for winner</th> <td>80%</td></tr>
+      <tr><th>Total Amount deposited</th> <td>{totalDeposits}</td></tr>
+      <tr><th>Your deposit in Pool</th> <td>{yourDeposit}</td></tr>
 
-      <li>Total Amount deposited: {totalDeposits}</li>
-      <li>Your deposit in Pool: {yourDeposit}</li>
       {state!=="Active" ? 
-      <><li>Total Tickets Circulated: {ticketsCirculated}</li>
-      <li>Your tickets in Pool: {yourTickets}</li></>: <></>}
+      <>
+      <tr><th>Total Tickets Circulated</th> <td>{ticketsCirculated*(10**18)}</td></tr>
+      <tr><th>Your tickets in Pool</th> <td>{yourTickets*(10**18)}</td></tr>
+      </>: <></>}
       {state==="Closed" ?
-      <><li>Are you a winner: {winner?"Yes":"No"}</li>
-      <li>Total Interest accured: {totalInterest}</li>
-      <li>Your Prize to Claim: {yourPrize}</li></>: <></>}
-    </ul>
-    </div>
+      <>
+      <tr><th>Total Interest accured</th> <td> {totalInterest}</td></tr>
+      <tr><th>Your Prize to Claim</th> <td> {yourPrize}</td></tr></>: <></>}
 
-    <div className="col poolButtons">
-        {state!=="Active" ?
+    <tr>
+        <td>{state!=="Active" ?
         (state==="Locked" ? 
         <></> :
-        <button type="button" className="btn btn-primary" onClick={claimPrize}> Claim Your Prize! </button> ) :
-        <button type="button" className="btn btn-primary" onClick={depositFunds}> Deposit </button> }
-
-    </div>
+        (yourTickets===0 ? <button type="button" className="btn btn-primary" onClick={claimPrize}> Claim Your Prize! </button> : <button type="button" className="btn btn-primary" onClick={claimPrize} disabled> Claimed </button>) ) :
+        <button type="button" className="btn btn-primary" onClick={depositFunds}> Deposit 1 ETH </button> }
+        </td>
+        <td>{address===PRIZE_POOL_OWNER_ADDRESS ? <>
+          {state==="Locked"?<>
+          <button type="button" className="btn btn-primary" onClick={depositToVault}> Deposit to Yearn Vault </button>
+          </>:<>
+          <button type="button" className="btn btn-primary" onClick={depositToVault} disabled> Deposit to Yearn Vault </button>
+          </>}
+        </> : <></>}</td>
+        <td>{address===PRIZE_POOL_OWNER_ADDRESS ? <>
+          {state==="Closed"?<>
+          <button type="button" className="btn btn-primary" onClick={withdrawFromVault}> Withdraw from yearn vault </button>
+          </>:<>
+          <button type="button" className="btn btn-primary" onClick={withdrawFromVault} disabled> Withdraw from Yearn Vault </button>
+          </>}
+        </> : <></>}</td>
+    </tr>
+    </table>
 
     </div>}
     </>
